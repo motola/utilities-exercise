724c5ebd13c0a5b85366c5d2630e45248f3dc7e8
motola <olutolamoses@gmailcom>
2022-05-15 17:09:38 -0700

    utility libraries and methods

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+import {largeNumber, cut3} from './utility/ulib1.js';
+import {sum, concat} from './utility/ulib2.js'
+
+
+let arr1 = [1,2,4]
+let arr2 = [3,5,6]
+
+
+let theLargeNo = largeNumber.bind(null, arr1);
+console.log('Largest Number in the Array: ', theLargeNo());
+
+console.log('3rd item from array removed: ', cut3(arr1));
+
+console.log('Total Numbers in the Array: ', sum(arr1));
+
+
+
+console.log('Array Concatenated: ', concat.call(this, arr1, arr2));
---
+++ utility/ulib2.js
@@ -0,0 +1,16 @@
+// add numbers in an array
+const sum = (arr) => {
+    let total = 0;
+    arr.forEach((x) => {
+      total += x;
+    })
+    return total
+  }
+  
+  // concatenate two arrays
+  const concat = (arr1, arr2) => {
+    return [...arr1, ...arr2]
+  }
+
+
+export {sum, concat}
